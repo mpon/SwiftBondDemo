396bc9b5525e3b61b028c86569a470dfdd810577
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2015-02-04 14:14:05 +0900

    Initial Commit

---
+++ SwiftBondDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBondDemo
+//
+//  Created by Masato OSHIMA on 2015/02/04.
+//  Copyright (c) 2015年 Masato OSHIMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
