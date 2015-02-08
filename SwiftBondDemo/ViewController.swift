4006bf040aa41cd0100a4146168bfbad30776da7
Masato Oshima <qqw75pn9@gmail.com>
2015-02-08 16:17:18 +0900

    Implemented validation

--- SwiftBondDemo/ViewController.swift
+++ SwiftBondDemo/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 import Bond
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak private var usernameField: UITextField!
+    @IBOutlet weak private var emailField: UITextField!
+    @IBOutlet weak private var passwordField: UITextField!
+    @IBOutlet weak private var passwordConfirmField: UITextField!
+    @IBOutlet weak private var errorLabel: UILabel!
+    @IBOutlet weak private var createButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+                
         // Do any additional setup after loading the view, typically from a nib.
+        let rule1a = reduce(usernameField, emailField, false) { username, email in
+            countElements(username) > 0 && countElements(email) > 0
+        }
+        
+        let rule1b = reduce(passwordField, passwordConfirmField, false) { password, confirmPassword in
+            password == confirmPassword
+        }
+        
+        let rule1c = passwordField.map {countElements($0) >= 8}
+                
+        reduce(rule1a, rule1b, rule1c, false) { rule1aOk, rule1bOk, rule1cOk in
+            rule1aOk && rule1bOk && rule1cOk
+        } ->> createButton
+        
+        reduce(passwordField, passwordConfirmField, "") { password, confirmPassword -> String in
+            password == confirmPassword ? "" : "The password does not match"
+        } ->> errorLabel
+        
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
